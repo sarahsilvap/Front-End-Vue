b422441e100a621280addac221b5fc4d896bf109
sarahsilvap <sarahs-pereira@outlook.com>
2024-09-05 21:53:41 -0300

    aula de vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,42 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <p>{{ mensagem }}</p>
+  <p>Contador: {{ contador }}</p>
+  <button @click='incrementar'>Clique</button>
+  <p v-if='exibir'>Texto visível</p>
+  <p></p>
+  <button @click='mostrar'>Clique para mostrar/ocultar texto</button>
+  <p></p>
+  <p>Lista não ordenada:</p>
+  <ul>
+    <li v-for="item in lista" :key="item">{{ item }}</li>
+  </ul>
+  <input v-model='nome' placeholder='Digite seu nome' />
+  <p>Nome: {{ nome }}</p>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      mensagem: 'Bem-vindo ao Vue.js!',
+      contador: 0,
+      exibir: false,
+      lista: ['Escola', 'Trabalho', 'Academia', 'Casa'],
+      nome: ''
+    }
+  },
+
+  methods: { 
+    incrementar() { 
+      this.contador++; 
+    },
+
+    mostrar() {
+      this.exibir = !this.exibir;
+    }
   }
 }
 </script>
@@ -23,4 +50,22 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+ul {
+  list-style-type: none; /* Remove os marcadores */
+  padding: 0; /* Remove o padding padrão */
+  margin: 0; /* Remove a margem padrão */
+}
+
+ul li {
+  padding: 10px 0; /* Adiciona espaçamento interno acima e abaixo */
+}
+
+ul li:not(:last-child) {
+  border-bottom: 1px solid #ddd; /* Adiciona uma linha entre os itens */
+}
+</style>
+
+<style scoped>
+  p {color: red;}
 </style>
